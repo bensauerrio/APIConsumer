ea82fd4d0dd536823f6b6d535b7884d061eaf5bd
Ben Sauer <Ben Sauer@DESKTOP-BG74MIU>
2020-09-11 20:28:57 -0300

    Commit Inicial

---
+++ src/test/java/com/yahoofinanceapiconsumer/YahooFinanceApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahoofinanceapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahooFinanceApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
